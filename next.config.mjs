b86c4d9dc42733223c8ed686da3b52bd1744a509
Diwa Oliq <diwaoliq@gmail.com>
2024-06-14 21:06:41 +0700

    login

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,22 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-    eslint:{
-        ignoreDuringBuilds: true,
+    webpack: (config, { isServer }) => {
+      config.module.rules.push({
+        test: /\.html$/,
+        use: 'html-loader'
+      });
+  
+      return config;
     },
-};
-
-export default nextConfig;
+    async redirects() {
+      return [
+        {
+          source: '/',
+          destination: '/login',
+          permanent: true,
+        },
+      ];
+    },
+  };
+    
+    export default nextConfig;
+    
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,23 @@
 import NextAuth from 'next-auth';
 import { authConfig } from './auth.config';
- 
-export default NextAuth(authConfig).auth;
- 
+import { NextRequest, NextResponse } from 'next/server';
+
+const auth = NextAuth(authConfig).auth;
+
+export default function middleware(req: NextRequest) {
+  const url = req.nextUrl.clone();
+
+  // Check if the request is for the root URL
+  if (url.pathname === '/') {
+    // Redirect to the login page
+    url.pathname = '/login';
+    return NextResponse.redirect(url);
+  }
+
+  // Call the NextAuth middleware for other requests
+  return auth(req);
+}
+
 export const config = {
   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
   matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
